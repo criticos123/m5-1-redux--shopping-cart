8127bf47089179976d35040670b24259eff73728
Stephanos Criticos <scritico@unb.ca>
2020-08-05 09:59:54 -0400

    exercise 1

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import App from './components/App';
-
-const rootElement = document.getElementById('root');
-
-ReactDOM.render(<App />, rootElement);
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import App from "./components/App";
+import reducer from "./reducers";
+const rootElement = document.getElementById("root");
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
